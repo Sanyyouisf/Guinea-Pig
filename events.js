a0c02271aee0c6e994da1872414d059172baf635
Sanyyouisf <sanymamdoh@yahoo.com>
2017-03-08 22:36:58 -0600

    finish

--- events.js
+++ events.js
@@ -3,6 +3,12 @@ var input = document.getElementById("keypress-input");
 var output = document.getElementById("output-target");
 var article = document.getElementsByClassName("article-section");
 var pageTitle = document.getElementById("page-title");
+var addColor = document.getElementById("add-color");
+var guineaPig = document.getElementById("guinea-pig");
+var makeLarge= document.getElementById("make-large");
+var addBorder= document.getElementById("add-border");
+var addRounding = document.getElementById("add-rounding");
+
 
 //When any section is clicked the output target text should be "You clicked on the {text of the section} section"
 //note we add the for loop here because i have more than one class with the same name
@@ -13,11 +19,13 @@ for (var i = 0; i < article.length; i++) {
 	});
 }
 
+
 //When the mouse is over the h1 tag, the output element should contain the text "You moved your mouse over the header".
 // note the event of mouse hover over is called mouseover
 pageTitle.addEventListener("mouseover",function(event){
 	output.innerHTML = "You moved your mouse over the header ";
 });
+
 
 //When the mouse leaves the h1 tag, the output element should contain the text "You left me!!".
 // note the event of mouse hover over is called mouseout
@@ -25,18 +33,37 @@ pageTitle.addEventListener("mouseout",function(event){
 	output.innerHTML = "You left me!!";
 });
 
+
 //When you type characters into the input field, the output element should mirror the text in the input field.
+input.addEventListener("keyup",function(){
+	output.innerHTML = input.value;
+});
 
-//When you click the "Add color" button, the guinea-pig element's text color should change to blue.
 
-//When you click the "Hulkify" button, the guinea-pig element's font size should become much larger.
+//When you click the "Add color" button,
+//the guinea-pig element's text color should change to blue.
+addColor.addEventListener("click",function(event){
+	guineaPig.classList.add("textBlue");
+});
 
-//When you click the "Capture it" button, the guinea-pig element should have a border added to it.
 
-//When you click the "Rounded" button, the guinea-pig element's border should become rounded.
+//When you click the "Hulkify" button, 
+//the guinea-pig element's font size should become much larger.
+addBorder.addEventListener("click",function(event){
+	guineaPig.classList.add("addBorder");
+});
 
-//The first section's text should be bold.
 
-//The last section's text should be bold and italicized.
+//When you click the "Capture it" button,
+// the guinea-pig element should have a border added to it.
+makeLarge.addEventListener("click",function(event){
+	guineaPig.classList.add("textLarge");
+});
 
-//Make the buttons stop appearing next to each other as inline elements. Change them into block elements.
+
+//When you click the "Rounded" button, 
+//the guinea-pig element's border should become rounded.
+addRounding.addEventListener("click",function(event){
+	guineaPig.classList.add("addRounding");
+});
+
